914123fb8b0c3f526313a263888f6a42166f2b1c
InsonusK <insonus.k@gmail.com>
2026-02-28 23:56:02 +0000

    add annotitons for di

---
+++ src/AnnotationsForDI/InsonusK.Shared.AnnotationsForDI.Test/AnnotationsForDITests.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace InsonusK.Shared.AnnotationsForDI.Test;
+
+[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public class AnnotationsForDITests
+{
+    [Fact]
+    public void ServiceAttribute_CanBeConstructed_WithDefaultValues()
+    {
+        var attr = new ServiceAttribute();
+        Assert.NotNull(attr);
+    }
+
+    [Fact]
+    public void ServiceAttribute_CanBeAppliedToClass()
+    {
+        var type = typeof(TestService);
+        var attr = type.GetCustomAttribute<ServiceAttribute>();
+        Assert.NotNull(attr);
+    }
+
+    [Fact]
+    public void ServiceCollectionExtensions_AddAnnotatedServices_RegistersService()
+    {
+        var services = new ServiceCollection();
+        services.AddAnnotatedServices(typeof(TestService).Assembly);
+
+        var provider = services.BuildServiceProvider();
+        var service = provider.GetService<ITestService>();
+        Assert.NotNull(service);
+        Assert.IsType<TestService>(service);
+    }
+
+    // Helper types for testing
+    [Service]
+    private class TestService : ITestService { }
+
+    private interface ITestService { }
+}
