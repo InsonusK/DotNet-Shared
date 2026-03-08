ae99c84fcb92d1115cd54343451ebb0f4667027b
InsonusK <insonus.k@gmail.com>
2026-03-08 19:10:06 +0000

    add documentation and up version

--- src/Command/Command.Service.Interface/InsonusK.Shared.Command.Service.Interface/Helpers/CommandContextSourceExtension.cs
+++ src/Command/Command.Service.Interface/InsonusK.Shared.Command.Service.Interface/Helpers/CommandContextSourceExtension.cs
@@ -4,8 +4,20 @@ using MediatR;
 
 namespace InsonusK.Shared.Command.Service.Interface.Helpers;
 
+/// <summary>
+/// Provides extension methods for <see cref="ICommandContextSource"/> to simplify entity retrieval.
+/// </summary>
 public static class CommandContextSourceExtensions
 {
+    /// <summary>
+    /// Gets an entity from the command context source using the provided command.
+    /// </summary>
+    /// <typeparam name="TCommand">The type of the command, must implement <see cref="ICommandWithEntityKeys"/> and <see cref="IBaseRequest"/>.</typeparam>
+    /// <typeparam name="TEntity">The type of the entity, must inherit from <see cref="EntityBase"/>.</typeparam>
+    /// <param name="source">The command context source.</param>
+    /// <param name="command">The command containing entity keys.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The loaded entity.</returns>
     public static async Task<TEntity> GetEntityAsync<TCommand, TEntity>(this ICommandContextSource source, TCommand command, CancellationToken cancellationToken)
         where TCommand : ICommandWithEntityKeys, IBaseRequest
         where TEntity : EntityBase
@@ -14,6 +26,15 @@ public static class CommandContextSourceExtensions
         return cmdContext.Get<TEntity>();    
     }   
 
+    /// <summary>
+    /// Attempts to get an entity from the command context source using the provided command.
+    /// </summary>
+    /// <typeparam name="TCommand">The type of the command, must implement <see cref="ICommandWithEntityKeys"/> and <see cref="IBaseRequest"/>.</typeparam>
+    /// <typeparam name="TEntity">The type of the entity, must inherit from <see cref="EntityBase"/>.</typeparam>
+    /// <param name="source">The command context source.</param>
+    /// <param name="command">The command containing entity keys.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The loaded entity or null if not found.</returns>
     public static async Task<TEntity?> TryGetEntityAsync<TCommand, TEntity>(this ICommandContextSource source, TCommand command, CancellationToken cancellationToken)
         where TCommand : ICommandWithEntityKeys, IBaseRequest
         where TEntity : EntityBase
